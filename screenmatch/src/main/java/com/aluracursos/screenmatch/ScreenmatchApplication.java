c66ad5c9830ee89cf442b5d4647dd616ec6bcc94
nestors42 <nestorsebastiangome.s42@gmail.com>
2024-05-28 17:45:12 -0300

    creando clases para mapear de api a java

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosEpisodios;
 import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -22,6 +27,18 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var datos = conversor.obtenerDatos(json, DatosSerie.class);
 //		System.out.println(json);
 		System.out.println(datos);
+		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=bb208c9");
+		DatosEpisodios episodios = conversor.obtenerDatos(json, DatosEpisodios.class);
+		System.out.println(episodios);
+
+		List<DatosTemporadas> temporadas = new ArrayList<>();
+		for (int i = 1; i <=datos.totalDeTemporadas() ; i++) {
+			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season="+i+"&apikey=bb208c9");
+			var datosTemporada = conversor.obtenerDatos(json, DatosTemporadas.class);
+			temporadas.add(datosTemporada);
+
+		}
+		temporadas.forEach(System.out::println);
 
 
 	}
